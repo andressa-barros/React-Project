160d128d2dff98a3202716be8baa17623c89cc4e
andressa-barros <andressa.barros4194@gmail.com>
2026-04-04 16:08:20 -0300

    Created react project
    Criação do projeto de react

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import Jogos from "./components/Jogos";
+
+function App() {
+  return (
+    <div>
+      <h1>Catálogo de Jogos</h1>
+      <Jogos />
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/components/Jogos.jsx
@@ -0,0 +1,14 @@
+function Jogos() {
+  return (
+    <div>
+      <h2>Lista de Jogos</h2>
+      <ul>
+        <li>Jogo 1</li>
+        <li>Jogo 2</li>
+        <li>Jogo 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Jogos;
